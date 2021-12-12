36fb7361a88cc868001e8163352242fd08b15eac
Jeff Vaz <jeff.j.vaz@gmail.com>
2021-12-11 17:01:19 -0800

    add functionality where snake grows after eating apple

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import * #importing event keywords
 import time #necessary for letting snake move on its own, but slowly
+import random #necessary to randomly move apple after eating
 
 SIZE = 40 #the size of each block that makes up the snake
 
@@ -16,6 +17,9 @@ class Apple:
     self.parent_screen.blit(self.image, (self.x, self.y))
     pygame.display.flip()
 
+  def move(self):
+    self.x = random.randint(1,24) * SIZE
+    self.y = random.randint(1,19) * SIZE
 
 class Snake:
   def __init__(self, parent_screen, length):
@@ -36,6 +40,11 @@ class Snake:
       self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
     
     pygame.display.flip()
+
+  def increase_length(self):
+    self.length += 1
+    self.x.append(-1)
+    self.y.append(-1)
 
   def move_left(self):
     self.direction = 'left'
@@ -76,25 +85,27 @@ class Game:
     #setting the window size and background of the main display
     self.surface = pygame.display.set_mode((1000,800))
     self.surface.fill((110,110,5))
-    self.snake = Snake(self.surface, 6) #creates snake inside of game class
+    self.snake = Snake(self.surface, 2) #creates snake inside of game class
     self.snake.draw() 
     self.apple = Apple(self.surface)
     self.apple.draw()
   
-  def is_collision(self, x1, x2, y1, y2):
-    if x1 >= x2 + SIZE and x1 <= x2 + SIZE:
-      if y1 >= x2 + SIZE and y1 <= x2 + SIZE:
+  def is_collision(self, x1, y1, x2, y2):
+    if x1 >= x2 and x1 < x2 + SIZE:
+      if y1 >= y2 and y1 < y2 + SIZE:
         return True
     return False
-
-
 
   def play(self):
     self.snake.walk()
     self.apple.draw()
 
     if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+      self.snake.increase_length()
+      self.apple.move()
       print('collision occured')
+
+
 
   def run(self):
     #set loop to continue playing game until the escape or X button is hit
@@ -122,7 +133,7 @@ class Game:
           running = False 
       
       self.play()
-      time.sleep(0.3) #every 0.3 seconds snake moves, needed it to slow down
+      time.sleep(0.1) #every 0.3 seconds snake moves, needed it to slow down
 
 
 if __name__ == "__main__":
